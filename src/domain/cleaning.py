a0b0a01dba3ed8de8e6acce368849fc560d6dff4
Dorota Bailly <dorota.bailly@gmail.com>
2020-11-18 20:09:57 +0100

    define methode group_to_category_etudiant

--- src/domain/cleaning.py
+++ src/domain/cleaning.py
@@ -61,7 +61,8 @@ class DataCleaner :
         df_without_outliers_errors = self._correct_outliers_errors(df_without_features_low_second_category)
         df_with_corrected_niveau_lead = self._correct_select_niveau_lead(df_without_outliers_errors)
         df_with_category_formulaire_add = self._add_category_formulaire_add(df_with_corrected_niveau_lead)
-        return df_with_category_formulaire_add
+        df_with_category_etudiant = self._group_to_category_etudiant(df_with_category_formulaire_add)
+        return df_with_category_etudiant
 
     @staticmethod
     def _drop_not_exploitable_features(df):
@@ -108,7 +109,13 @@ class DataCleaner :
         X[stg.ORIGINE_LEAD_COL] = X[stg.ORIGINE_LEAD_COL].replace( "formulaire lead add","formulaire add",)
         return X
 
+
+    @staticmethod
+    def _group_to_category_etudiant(df):
   
+        X = X.copy()
+        X[stg.NIVEAU_LEAD_COL] = X[stg.NIVEAU_LEAD_COL].replace( "etudiant d'une certaine ecole","etudiant")
+        return X
 
 
 
